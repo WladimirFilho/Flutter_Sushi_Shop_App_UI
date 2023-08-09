a50829d19597267b50c0feef042286390860032f
WladimirFilho <72136799+WladimirFilho@users.noreply.github.com>
2023-08-09 16:45:55 +1000

    first commit

---
+++ lib/components/my_button.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class MyButton extends StatelessWidget {
+  final String text;
+  const MyButton({super.key, required this.text});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(20),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(30),
+        color: Color(0xFFDB8882),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
+            text,
+            style: const TextStyle(
+              color: Colors.white,
+            ),
+          ),
+          Icon(
+            Icons.arrow_forward,
+            color: Colors.white,
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/pages/intro_page.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:sushi_app/components/my_button.dart';
+
+class IntroPage extends StatelessWidget {
+  const IntroPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFFA44740),
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.all(30.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              // shop name
+              Text(
+                'SUSHI MAN',
+                style: GoogleFonts.dmSerifDisplay(
+                  fontSize: 28,
+                  color: Colors.white,
+                ),
+              ),
+
+              // icon
+              Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Image.asset(
+                  'lib/assets/sushi(2).png',
+                ),
+              ),
+              const SizedBox(height: 10),
+
+              // title
+              Text(
+                'THE TASTE OF JAPANESE FOOD',
+                style: GoogleFonts.dmSerifDisplay(
+                  fontSize: 40,
+                  color: Colors.white,
+                ),
+              ),
+
+              const SizedBox(
+                height: 20,
+              ),
+
+              // subtitle
+              const Text(
+                'Feel the taste of the most popular Japanese food from anywhere and anytime',
+                style: TextStyle(color: Color(0xFFDFDFDF), height: 2),
+              ),
+
+              const SizedBox(
+                height: 20,
+              ),
+
+              MyButton(text: 'Get started')
+
+              // get started button
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
